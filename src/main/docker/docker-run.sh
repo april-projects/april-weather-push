4bf91f0676234ebe41fad48bc1b3ed7362deab72
mobaijun <mobaijun8@163.com>
2022-08-29 11:09:49 +0800

    :rocket: 添加dockerfile文件|添加简化操作脚本

---
+++ src/main/docker/docker-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker rm $(docker stop april-proxy)
+
+# 打包
+docker build -t april-proxy:1.0.0 .
+
+# run
+docker run --restart=always -dit -p 8999:8080 --name april-proxy mobaijun/april-weather-push:1.0.0
+
+# 提交
+# docker commit 7227510800df  mobaijun/april-weather-push:1.0.0
+
+# 推送
+# docker push mobaijun/april-weather-push:1.0.0
