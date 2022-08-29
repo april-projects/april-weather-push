e7999c874d08d4a494d93a1ff85c1b188c1c6b52
mobaijun <mobaijun8@163.com>
2022-08-29 12:41:53 +0800

    :art: 修改images名称

--- src/main/docker/docker-run.sh
+++ src/main/docker/docker-run.sh
@@ -3,7 +3,7 @@
 docker rm $(docker stop april-proxy)
 
 # 打包
-docker build -t april-proxy:1.0.0 .
+docker build -t april-weather-push:1.0.0 .
 
 # run
 docker run --restart=always -dit -p 8999:8080 --name april-proxy mobaijun/april-weather-push:1.0.0
